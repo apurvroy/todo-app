c79794cd43d3af691d201184e60c7b571ffbf0a8
apurv roy <apurvroy010@gmail.com>
2019-10-14 01:19:24 +0530

    store and show the tasks

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,15 @@
 const List=require('../models/list');
 module.exports.home=function(req,res){
+    List.find({},function(err,tasks){
+        if(err){
+          console.log('erroe in fetching the contacts from db');
+          return ;
+        };
     return res.render('home',{
-        title:"TODO"
+        title:"TODO",
+        list:tasks
     });
+});
 };
 
 module.exports.create=function(req,res){
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,9 @@ const express =require('express');
 const router=express.Router();
 const homeController=require('../controllers/home_controller');
 
-
 router.get('/',homeController.home);
 
-router.post('/create-list',homeController.create);
+router.post('/create',homeController.create);
 
 
 
